fa8dafb3cc280694dcb03dfdf30681fbf642c17c
jakub <jkl.school6@gmail.com>
2022-12-05 23:38:59 +0100

    Dodano: - zadanie2 To-do - upiekszenie kodu

---
+++ src/zad2/Zadanie2.java
@@ -0,0 +1,157 @@
+package zad2;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Zadanie2 {
+    public static void main(String[] args) {
+        new MyFrame();
+    }
+}
+
+class MyFrame extends JFrame implements ActionListener {
+    final int N = 10;
+    List<JTextField> polaTekstowe = Methods.init_polaTekstowe(N);
+    List<JCheckBox> checkBoxy = Methods.init_checkBoxy(N);
+    List<JLabel> labelTest = Methods.init_labelTest(N);
+    List<Integer> wysokosciSlupkow;
+    List<Slupek> slupeks = Methods.init_Slupki(N);
+    ChartPanel chart;
+    List<Integer> inputs = Methods.init_inputs(N);
+    int x=130;
+    int y=100;
+    public MyFrame(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1000,700);
+        for(int i=0;i<polaTekstowe.size();i++){
+            polaTekstowe.get(i).setBounds(x,y,50,15);
+            checkBoxy.get(i).setBounds(x+70,y-18,50,50);
+            labelTest.get(i).setBounds(300,y,150,15);
+            y+=40;
+            checkBoxy.get(i).addActionListener(this);
+            add(polaTekstowe.get(i));
+            add(checkBoxy.get(i));
+            add(labelTest.get(i));
+        }
+        setLayout(null);
+        setVisible(true);
+        setLocationRelativeTo(null);
+    }
+    public void przemaluj(){
+        chart = new ChartPanel(slupeks);
+        add(chart);
+        chart.repaint();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        for(int i=0;i<checkBoxy.size();i++){
+            if(checkBoxy.get(i).isSelected()){
+                System.out.println("selected" + i);
+                try{
+                    int input = Integer.parseInt(polaTekstowe.get(i).getText());
+                    inputs.set(i,input);
+                    System.out.println(inputs.get(i));
+                } catch (NumberFormatException ex){
+                    System.out.println("Wrong input.");
+                }
+            } else inputs.set(i,0);
+        }
+        wysokosciSlupkow = Methods.obliczanie_Wysokosci(inputs,N);
+        for(int i=0;i<slupeks.size();i++) slupeks.set(i,new Slupek(wysokosciSlupkow.get(i),slupeks.get(i).color));
+        przemaluj();
+    }
+
+}
+
+class ChartPanel extends JPanel{
+    int x = 10;
+    int y = 0;
+    List<Slupek> slupeks;
+    public ChartPanel(List<Slupek> slupki) {
+        this.slupeks = slupki;
+        this.setBounds(470,170,400,400);
+        this.setBorder(BorderFactory.createLineBorder(Color.black));
+        setVisible(true);
+    }
+    public void paint(Graphics g){
+        super.paint(g);
+        Graphics2D g2D = (Graphics2D) g;
+        for(int i=0;i<slupeks.size();i++){
+            if(slupeks.get(i).height!=0){
+                y=400-slupeks.get(i).height*3;
+                System.out.println("XD");
+                g2D.setPaint(slupeks.get(i).color);
+                g2D.fillRect(x,y,30,slupeks.get(i).height*3);
+                System.out.println(x);
+                System.out.println(y);
+                System.out.println(slupeks.get(i).height*3);
+                x+=40;
+            }
+        }
+    }
+}
+
+class Methods{
+    public static List<JTextField> init_polaTekstowe(int n){
+        List<JTextField> pola = new ArrayList<>();
+        for(int i=0;i<n;i++) pola.add(new JTextField());
+        return pola;
+    }
+    public static List<JCheckBox> init_checkBoxy(int n){
+        List<JCheckBox> checkBoxes = new ArrayList<>();
+        for(int i=0;i<n;i++) checkBoxes.add(new JCheckBox());
+        return checkBoxes;
+    }
+    public static List<JLabel> init_labelTest(int n){
+        List<JLabel> checkBoxes = new ArrayList<>();
+        for(int i=0;i<n;i++) checkBoxes.add(new JLabel());
+        return checkBoxes;
+    }
+    public static List<Slupek> init_Slupki(int n){
+        Random r = new Random();
+        List<Slupek> slupeks = new ArrayList<>();
+        for(int i=0;i<n;i++) slupeks.add(new Slupek(0,new Color(r.nextInt(256), r.nextInt(256), r.nextInt(256), r.nextInt(256))));
+        return slupeks;
+    }
+    public static List<Integer> init_inputs(int n){
+        List<Integer> wysokosci = new ArrayList<>();
+        for(int i=0;i<n;i++) wysokosci.add(0);
+        return wysokosci;
+    }
+    public static List<Integer> obliczanie_Wysokosci(List<Integer> inputs, int n){
+        List<Integer> wysokosci = new ArrayList<>();
+        double XD;
+        for(int i=0;i<n;i++) wysokosci.add(0);
+        int calosc=0;
+        for(int i=0;i<n;i++) calosc+=inputs.get(i);
+        System.out.println(calosc);
+        for(int i=0;i<n;i++) {
+            try {
+                XD = (double) inputs.get(i) / calosc *100;
+                wysokosci.set(i, (int) XD);
+                System.out.println(wysokosci.get(i));
+            } catch (ArithmeticException e) {
+                System.out.println("NO DIVIDO THRU ZERO MEINE FREUNDE!");
+                wysokosci.set(i,0);
+                System.out.println(wysokosci.get(i));
+            }
+        }
+        return wysokosci;
+    }
+}
+
+class Slupek{
+    public int height;
+    public Color color;
+
+    public Slupek(int wysokosc, Color kolor){
+    this.height = wysokosc;
+    this.color = kolor;
+    }
+}
